0d6c18d9d7a9941f26b6f0cc26cc74b75988f36d
twozeros <pak.kiruha@yandex.ru>
2023-10-15 14:21:22 +0300

    refactor

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ let taskList = []
 class Task {
     constructor(text ) {
         this.text = text;
+        this.id = this.getNextId()
+        this.complete = false
+    }
+    getNextId() {
+        let counter =+localStorage.getItem("counter")
+        if(counter == null) {
+            counter = 0
+            localStorage.setItem("counter", 1)
+            return counter    
+        }
+        localStorage.setItem("counter", counter +1)
+        return counter
     }
 }
 function addTask() {
@@ -14,6 +26,7 @@ function addTask() {
         li.innerHTML = inputBox.value;        
         let t = new Task(li.textContent)
         taskList.push(t)
+        li.setAttribute("data-id", t.id);
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
@@ -26,10 +39,15 @@ function addTask() {
 listContainer.addEventListener('click', function(event) {
     if(event.target.tagName === 'LI') {
         event.target.classList.toggle('checked');
+        let id = event.target.getAttribute("data-id")
+        let task = taskList.find(x => x.id == +id)
+        task.complete = !task.complete
         saveData();
     } 
     else if (event.target.tagName === 'SPAN') {
         event.target.parentElement.remove();
+        let id = event.target.parentElement.getAttribute("data-id")
+        taskList = taskList.filter( x=>x.id != id)
         saveData();
     }
 }, false)
@@ -40,10 +58,18 @@ function saveData() {
 
 function showTask() {
     let json = localStorage.getItem("task-db");
-    const arr = JSON.parse(json);
-    arr.forEach(function(element, key){
+    if(json!=null) {
+        taskList = JSON.parse(json);
+    }    
+    taskList.forEach(function(element, key){
         let li = document.createElement("li");
         li.innerHTML = element.text
+        li.setAttribute("data-id", element.id);
+        console.log(element)
+        if(element.complete == true) {
+            console.log(element.complete)
+            li.classList.toggle("checked")
+        }
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
